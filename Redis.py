0956bb14173eecbfce228fa14eb66ff2a0233060
Ahsan Shaikh <91524954+pyDashNinja@users.noreply.github.com>
2023-11-24 11:54:09 +0500

    Update Redis.py

--- Redis.py
+++ Redis.py
@@ -26,8 +26,6 @@ from diffusers import (
     AutoencoderKL,
     DDIMScheduler,
     StableDiffusionInpaintPipelineLegacy
-    # StableDiffusionUpscalePipeline
-
 )
 from transformers import pipeline
 from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
@@ -46,24 +44,16 @@ MODEL_CACHE = "cache"
 VAE_CACHE = "vae-cache"
 
 vae = AutoencoderKL.from_single_file(
-"https://huggingface.co/stabilityai/sd-vae-ft-mse-original/blob/main/vae-ft-mse-840000-ema-pruned.safetensors",
-torch_dtype=torch.float16
-#       "https://huggingface.co/Justin-Choo/epiCRealism-Natural_Sin_RC1_VAE/blob/main/epicrealism_naturalSinRC1VAE.safetensors"
-#            "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/blob/main/vae-ft-mse-840000-ema-pruned.safetensors",
-#            cache_dir=VAE_CACHE
+    "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/blob/main/vae-ft-mse-840000-ema-pruned.safetensors",
+    torch_dtype=torch.float16
         )
 pipe = StableDiffusionPipeline.from_pretrained(
-            MODEL_NAME,
-            vae=vae,
-            torch_dtype=torch.float16,
-            safety_checker=None
+    MODEL_NAME,
+    vae=vae,
+    torch_dtype=torch.float16,
+    safety_checker=None
 )
 
-#pipe = StableDiffusionPipeline.from_pretrained(
-#        MODEL_NAME, torch_dtype=torch.float16, safety_checker=None)
-##pipe.scheduler = DPMSolverMultistepScheduler.from_config(
-#    pipe.scheduler.config, use_karras_sigmas=True
-#)
 pipe.scheduler = DDIMScheduler(
     num_train_timesteps=1000,
     beta_start=0.00085,
@@ -73,12 +63,8 @@ pipe.scheduler = DDIMScheduler(
     set_alpha_to_one=False,
     steps_offset=1,
 )
-#vae = AutoencoderKL.from_pretrained(VAE_NAME)
-#print("pipe vae", pipe.vae)
-#pipe.vae = vae
-# pipe.enable_model_cpu_offload()
-pipe.enable_xformers_memory_efficient_attention()
 
+pipe.enable_xformers_memory_efficient_attention()
 pipe = pipe.to("cuda")
 
 
@@ -106,26 +92,8 @@ pipe_two = pipe_two.to("cuda")
 
 image_encoder_path = "./models/image_encoder/"
 ip_ckpt = "./models/ip-adapter-plus-face_sd15.bin"
-#ip_ckpt = "models/ip-adapter_sd15.bin"
 device = "cuda"
-#checkpoint = "lllyasviel/sd-controlnet-canny"
-#depth_estimator = pipeline('depth-estimation')
-#controlnet = ControlNetModel.from_pretrained(
-#   checkpoint, torch_dtype=torch.float16)
-#control = StableDiffusionControlNetPipeline.from_pretrained(
-#    MODEL_NAME, controlnet=controlnet, vae=vae, torch_dtype=torch.float16 ,safety_checker=None
-#)
-
-#control.scheduler = UniPCMultistepScheduler.from_config(
-#    control.scheduler.config)
-#control.scheduler = EulerAncestralDiscreteScheduler(
-#                beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear"
-#            )
-#control.enable_model_cpu_offload()
-#control.enable_xformers_memory_efficient_attention()
-#control = control.to("cuda")
 ip_model = IPAdapterPlus(pipe_two, image_encoder_path, ip_ckpt, device, num_tokens=16)
-#ip_model = []
 
 def process_request(data):
     try:
@@ -179,12 +147,6 @@ def process_request(data):
 
             except Exception as E:
                 print("Exception", E)
-                # lora_strength = 1
-                # lora_filename = "add_detail.safetensors"
-                # pipe.load_lora_weights(
-                #         f"./lora/{lora_filename.split('.')[0]}", weight_name=lora_filename)
-                # lora_prompt = "<lora:add_detail:" + \
-                #     str(lora_strength) + ">, "
                 lora_prompt = ""
 
             try:
@@ -196,80 +158,14 @@ def process_request(data):
             print(height, width, "height and width")
             # if len(prompt:
             prompt = lora_prompt + prompt
-                # " , | ultra highly detailed | masterpiece | 8K | cinematic | focused | high quality | hard focus, smooth, depth of field, 8K UHD"
-            # " , | ultra highly detailed | masterpiece | 8K | cinematic | focused | high quality, studio shoot, Nikon D850"
-            # compel = Compel(tokenizer=pipe.tokenizer,
-            #                 text_encoder=pipe.text_encoder)
-            # prompt_embeds = compel.build_conditioning_tensor(
-            #     prompt) if prompt else None
-            print(prompt,"checkign prompt now")
             image = pipe(prompt=prompt, negative_prompt=nprompt, width=width, height=height,
                          guidance_scale=gscale, num_inference_steps=steps, generator=generator).images[0]
 
             pipe.unload_lora_weights()
-
-            # low_threshold = 100
-            # high_threshold = 200
-
-            # # convert image into numpy
-            # image = np.array(image)
-
-            # image = cv2.Canny(image, low_threshold, high_threshold)
-            # image = image[:, :, None]
-            # image = np.concatenate([image, image, image], axis=2)
-            # control_image = Image.fromarray(image)
-
-            # print(prompt, "prompt")
-            # # try:
-            # #     height = int(ast.literal_eval(data['height']))
-            # #     width = int(ast.literal_eval(data['width']))
-            # # except:
-            # height = 1024
-            # width = 1024
-            # # print(height, width, "height and width")
-
-            # # image.save("sd-generated-image.jpeg")
-            # # image.save('sd-generated-image.jpeg',
-            # #            optimize=True, quality=80)
-            # image = control(prompt=prompt, negative_prompt=nprompt, width=width, height=height, guidance_scale=gscale, num_inference_steps=steps,
-            #                 generator=generator, image=control_image).images[0]
-
-        # read image as pillow and convert it to base64string image
+            
         elif model == 'control':
             image = data['control_image']
-            #low = int(ast.literal_eval(data['low']))
-            #high = int(ast.literal_eval(data['high']))
-            #guessmode = bool(data['guessmode'])
-
-            #try:
-            #    lora = str(data['lora'])#
-
-            #    if lora:
-            #        lora_strength = float(data['lora_strength'])
-            #        lora_filename = f"{lora}.safetensors"
-            #        control.load_lora_weights(
-            #            f"./lora/{lora}", weight_name=lora_filename)
-            #        lora_prompt = ""
-
-#                    if lora == 'add_detail':
-#                        lora_prompt = "<lora:add_detail:" + \
-#                            str(lora_strength) + ">, "
-                            
-#                else:
-#                    lora_prompt = ""
-
- #           except:
-                # lora_strength = 1
-                # lora_filename = "add_detail.safetensors"
-                # control.load_lora_weights(
-                #     "OedoSoldier/detail-tweaker-lora", weight_name=lora_filename)
-                # lora_prompt = "<lora:add_detail:" + \
-                #     str(lora_strength) + ">, "
-#                lora_prompt = ""
-#            print("prompt 1", prompt)
-#            print("lora prompt", lora_prompt)
-
-            # convert image from base64string to PIL image
+           
             image = Image.open(BytesIO(base64.b64decode(image)))
             width, height = image.size
             if width*height >= 1024*1024:
@@ -285,180 +181,28 @@ def process_request(data):
                 height = height - (height % 8)
               image = image.resize((width, height))
               print(image.size,"image size")
-            # # get the image width and height
-            # width, height = image.size
-            # if width*height > 3000*3000:
-            #     width = int(width*0.2)
-            #     height = int(height*0.2)
-            # elif width*height > 2000*2000:
-            #     width = int(width*0.4)
-            #     height = int(height*0.4)
-            # elif width*height > 1000*1000:
-            #     width = int(width*0.6)
-            #     height = int(height*0.6)
-            # elif width*height > 500*500:
-            #     width = int(width*0.8)
-            #     height = int(height*0.8)
-            # print(width, height, "new width and height")
-            # # check if height and width is divisible by 8
-            # if width % 8 != 0:
-            #     width = width - (width % 8)
-            # if height % 8 != 0:
-            #     height = height - (height % 8)
 
-            # image = image.resize((width, height))
-
-            # image = depth_estimator(image)['predicted_depth'][0]
-            # image = image.numpy()
-
-            # image_depth = image.copy()
-            # image_depth -= np.min(image_depth)
-            # image_depth /= np.max(image_depth)
-
-            # bg_threhold = 0.4
-
-            # x = cv2.Sobel(image, cv2.CV_32F, 1, 0, ksize=3)
-            # x[image_depth < bg_threhold] = 0
-
-            # y = cv2.Sobel(image, cv2.CV_32F, 0, 1, ksize=3)
-            # y[image_depth < bg_threhold] = 0
-
-            # z = np.ones_like(x) * np.pi * 2.0
-
-            # image = np.stack([x, y, z], axis=2)
-            # image /= np.sum(image ** 2.0, axis=2, keepdims=True) ** 0.5
-            # image = (image * 127.5 + 127.5).clip(0, 255).astype(np.uint8)
-            # image = Image.fromarray(image)
-            #image = np.array(image)
-            #image = depth_estimator(image)['depth']
-            #image = np.array(image)
-            #image = image[:, :, None]
-            #image = np.concatenate([image, image, image], axis=2)
-            #control_image = Image.fromarray(image)
-            try:
-                height = int(ast.literal_eval(data['height']))
-                width = int(ast.literal_eval(data['width']))
-            except:
-                height = 512
-                width = 512
-            #ip_model = IPAdapter(pipe, image_encoder_path, ip_ckpt, device)
-            #image = ip_model.generate(pil_image=image, num_samples=1, num_inference_steps=35, seed=42)
-            masked_image = Image.open('masked_image.png').convert("RGB")
-            mask = Image.open('mask.png').convert('RGB')
-            #rgb_image = Image.new("RGB", mask.size, (255, 255, 255))
-
-            # Paste the original image onto the new RGB image, using the alpha channel as a mask
-            #rgb_image.paste(mask, mask=mask.split()[3])
-            #gray_image = mask.convert("L")
-
-            # Convert the grayscale image to binary black and white
-            #mask = gray_image.point(lambda x: 0 if x < 128 else 255, '1').convert("RGB")
-
-            #images = ip_model.generate(pil_image=image, num_samples=4, num_inference_steps=, seed=42)
+            # find the image name
+            template_masked_image = f'./template/{str(data['template'])}/masked_image.png'
+            template_mask = f'./template/{str(data['template'])}/mask.png'
+            masked_image = Image.open(template_masked_image).convert("RGB")
+            mask = Image.open(template_mask).convert('RGB')
+            
             image = ip_model.generate(pil_image=image, num_samples=1, num_inference_steps=steps, seed=seed,
                 image=masked_image,mask_image=mask , strength=0.7)
-#pil_image=image, num_samples=4, num_inference_steps=50,
-#                           seed=42, image=masked_image, mask_image=mask, strength=0.7, 
-#, width=width, height=height)
-            #low_threshold = low
-            #high_threshold = high
 
-            #image = cv2.Canny(image, low_threshold, high_threshold)
-            #image = image[:, :, None]
-            #image = np.concatenate([image, image, image], axis=2)
-            #control_image = Image.fromarray(image)
-            #control_image.save('sd-generated-image.jpeg',
-             #                  optimize=True, quality=80)
-            #print("controlnet")
-            #prompt = lora_prompt + prompt
-            #print(prompt, "prompt")
-            #try:
-            #    height = int(ast.literal_eval(data['height']))
-            #    width = int(ast.literal_eval(data['width']))
-            #except:
-            #    height = 512
-            #    width = 512
             print(height, width, "height and width")
 
-            # image.save("sd-generated-image.jpeg")
-            # image.save('sd-generated-image.jpeg',
-            #            optimize=True, quality=80)
-            # compel = Compel(tokenizer=pipe.tokenizer,
-            #                 text_encoder=pipe.text_encoder)
-            # prompt_embeds = compel.build_conditioning_tensor(
-            #     prompt) if prompt else None
-            #image = control(prompt=prompt, negative_prompt=nprompt, width=width, height=height, guidance_scale=gscale, num_inference_steps=steps,
-            #                generator=generator, guess_mode=guessmode, image=control_image).images[0]
-            # image = control(prompt=prompt, negative_prompt=nprompt, width=width, height=height, guidance_scale=gscale, num_inference_steps=steps,
-            #
-#                 generator=generator, image=image).images[0]
             try:
                 image = image[0]
             except:
                 pass
             print(image,"image")
 
-        # elif model == 'upscale':
-        #     print(model, "model")
-        #     low_res_img = data['low_res_image']
-        #     try:
-        #         target_size = int(data['target_size'])
-        #         constant = int(data['constant'])
-        #         low_res_img = Image.open(
-        #             BytesIO(base64.b64decode(low_res_img)))
-        #         low_res_img = ImageResize(low_res_img, target_size, constant)
-        #     except:
-        #         low_res_img = Image.open(
-        #             BytesIO(base64.b64decode(low_res_img)))
-
-        #     # print( low res image size )
-        #     print(low_res_img.size, "image size ---")
-
-        #     try:
-        #         lora = str(data['lora'])
-
-        #         if lora == "add_detail":
-        #             lora_strength = float(data['lora_strength'])
-        #             lora_filename = "add_detail.safetensors"
-        #             control.load_lora_weights(
-        #                 "OedoSoldier/detail-tweaker-lora", weight_name=lora_filename)
-
-        #             lora_prompt = "<lora:add_detail:" + \
-        #                 str(lora_strength) + ">, "
-        #         else:
-        #             lora_prompt = ""
-
-        #     except:
-        #         # lora_strength = 1
-        #         # lora_filename = "add_detail.safetensors"
-        #         # control.load_lora_weights(
-        #         #     "OedoSoldier/detail-tweaker-lora", weight_name=lora_filename)
-        #         # lora_prompt = "<lora:add_detail:" + \
-        #         #     str(lora_strength) + ">, "
-        #         lora_prompt = ""
-
-        #     print("prompt 1", prompt)
-        #     print("lora prompt", lora_prompt)
-        #     prompt = lora_prompt + prompt
-        #     image = upscale(prompt=prompt, negative_prompt=nprompt, guidance_scale=gscale,
-        #                     num_inference_steps=steps, generator=generator, image=low_res_img).images[0]
-
-        # image = Image.open('sd-generated-image.png')
-        # convert image into webp
+        
         image = image.convert("RGB")
         image.save(name)
     except Exception as E:
-        # write error to a csv file with the name of the image, timestamp, error using pandas
-        #try:
-        #    df = pd.read_csv('error.csv')
-        #except:
-        #    df = pd.DataFrame(columns=['name', 'timestamp', 'error'])
-
-        # append row to the dataframe
-        #df = df.append({'name': name, 'timestamp': time.time(), 'error': E}, ignore_index=True)
-
-        # overwrite the csv file
-        #df.to_csv('error.csv', index=False)
         print(E,"exception")
 
 def worker():
